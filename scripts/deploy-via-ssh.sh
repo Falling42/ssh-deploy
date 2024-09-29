22dfdb2ec1bf5358b9c8bc170588a1ea25be1cdd
Falling42 <git@falling42.net>
2024-09-29 19:05:51 +0800

    Update deploy-via-ssh.sh

--- scripts/deploy-via-ssh.sh
+++ scripts/deploy-via-ssh.sh
@@ -29,11 +29,14 @@ check_param() {
   if [ -z "$param_value" ]; then
     echo "Error: $param_name is missing."
     exit 1
+  else
+    echo "$param_name is $param_value."
   fi
 }
 
 ssh_init(){
   mkdir -p ~/.ssh/
+  chmod 700 ~/.ssh/
 }
 
 # 设置 SSH 私钥
@@ -55,7 +58,6 @@ setup_ssh_config() {
   local ssh_port="${5:-22}"
   local proxy_jump="$6"
 
-  # 写入 SSH 配置
 if ! grep -q "Host $host_name" ~/.ssh/config; then
   echo "Writing SSH configuration for $host_name"
   cat >>~/.ssh/config <<END
@@ -98,8 +100,8 @@ check_and_install_screen() {
   fi
 }
 
-# 通过 screen 执行命令
-execute_command_with_screen() {
+# 执行命令
+execute_command() {
   local command="$1"
   local timestamp=$(date +'%Y%m%d%H%M%S')
   local screen_name="${timestamp}"
@@ -120,18 +122,14 @@ execute_command_with_screen() {
 # 确保远程目录存在
 ensure_directory_exists() {
   local remote_dir_path="$1"
+  
   echo "Checking if directory ${remote_dir_path} exists on remote host..."
-
-  # 检查远程目录是否存在
-  if ! execute_command_with_screen "[ -d ${remote_dir_path} ]"; then
+  if ! execute_command "[ -d ${remote_dir_path} ]"; then
     echo "Directory ${remote_dir_path} does not exist. Creating it..."
-    
-    # 如果不存在，则创建目录
-    if ! execute_command_with_screen "sudo mkdir -p ${remote_dir_path}"; then
+    if ! execute_command "sudo mkdir -p ${remote_dir_path}"; then
       echo "Error: Failed to create directory ${remote_dir_path}."
       exit 1
     fi
-
     echo "Directory ${remote_dir_path} created successfully."
   else
     echo "Directory ${remote_dir_path} already exists."
@@ -142,7 +140,7 @@ ensure_directory_exists() {
 set_file_permissions() {
   local remote_file_path="$1"
   echo "Setting file permissions for ${remote_file_path} on remote host..."
-  execute_command_with_screen "sudo chmod -R 755 ${remote_file_path}" || { echo "Error: Failed to set file permissions for ${remote_file_path}."; exit 1; }
+  execute_command "sudo chmod -R 755 ${remote_file_path}" || { echo "Error: Failed to set file permissions for ${remote_file_path}."; exit 1; }
   echo "File permissions for ${remote_file_path} set to 755 successfully."
 }
 
@@ -168,8 +166,8 @@ execute_deployment() {
   echo "Deployment executed successfully."
 }
 
+# 检查必需的参数
 check_required_params(){
-  # 检查必须参数
   check_param "$USE_SCREEN" "Use screen"
   check_param "$USE_JUMP_HOST" "Use jump host"
   check_param "$SSH_PRIVATE_KEY" "SSH private key"
@@ -180,8 +178,8 @@ check_required_params(){
   check_param "$TRANSFER_FILES" "Transfer files"
 }
 
+# 设置 SSH 环境
 setup_ssh(){
-  # 配置ssh
   ssh_init
   if [ "$USE_JUMP_HOST" == "yes" ]; then
     check_param "$JUMP_SSH_HOST" "Jump SSH host"
@@ -199,43 +197,37 @@ setup_ssh(){
   ls -al /home/runner/.ssh/
 }
 
+# 处理文件传输
 check_transfer_files(){
-  # 检查是否需要传输文件
   if [ "$TRANSFER_FILES" == "yes" ]; then
     check_param "$SOURCE_FILE_PATH" "Source file path"
     check_param "$DESTINATION_PATH" "Destination path"
-    
-    # 使用配置文件中的主机信息传输文件
     transfer_files "$SOURCE_FILE_PATH" "$DESTINATION_PATH" "remote"
   else
     echo "Skipping transfer files as per configuration."
   fi    
 }
 
+# 处理部署
 check_execute_deployment(){
-  # 检查是否执行远程脚本
   if [ "$EXECUTE_REMOTE_SCRIPT" == "yes" ]; then
     check_param "$COPY_SCRIPT" "Copy script"
     check_param "$DEPLOY_SCRIPT" "Deploy script"
-    # check_param "$SERVICE_NAME" "Service name"
-    # check_param "$SERVICE_VERSION" "Service version"
 
     if [ "$COPY_SCRIPT" == "yes" ]; then
       check_param "$SOURCE_SCRIPT" "Source script"
       ensure_directory_exists "$(dirname "$DEPLOY_SCRIPT")"
-      # 复制脚本
       transfer_files "$SOURCE_SCRIPT" "$DEPLOY_SCRIPT" "remote"
     fi
 
-    # 确保脚本具有执行权限
     set_file_permissions "$DEPLOY_SCRIPT" 
-    # 执行远程部署
     execute_deployment "$DEPLOY_SCRIPT" "$SERVICE_NAME" "$SERVICE_VERSION"
   else
     echo "Skipping remote script execution as per configuration."
   fi  
 }
 
+# 主函数
 main(){
   check_required_params
   setup_ssh
